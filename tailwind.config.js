768832653ca61f5cee1324a17a5c602d3fe0739a
diegodiego794 <60115301+diegodiego794@users.noreply.github.com>
2026-02-08 18:33:33 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./styles/**/*.{js,ts,jsx,tsx}"
+    "./styles/**/*.{css}"
   ],
   theme: {
     extend: {},
